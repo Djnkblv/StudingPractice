6307d80f63686150697d415e80e95ebe69c0ca3d
Djnkblv <djnkblv@gmail.com>
2023-07-11 15:42:06 +0700

    Урок #3. Добавил комментарии

--- StudingPractice/StudingPractice.cpp
+++ StudingPractice/StudingPractice.cpp
@@ -1,8 +1,9 @@
-﻿#include <iostream>
-using namespace std;
+﻿#include <iostream>		//директива include подключает библиотеку iostream
+using namespace std;	//пространство имен, для сокращения кода
 
 int main()
 {
+	//Стандартный код приветствия на русском языке.
 	setlocale(LC_ALL, "Rus");
 	cout << "Привет епта!" << endl;
 }
